07b730352a4544a1752f625358cdcc7494d4a2e0
Szabolcs Barnabás <szabolcs.barnabas@gmail.com>
2021-05-23 18:20:51 +0200

    Adds plot component with non-interactive plotting.

--- quasargui/components.py
+++ quasargui/components.py
@@ -1,11 +1,35 @@
 # noinspection PyUnresolvedReferences
-from ._base import Model, Component, ComponentWithModel
-# noinspection PyUnresolvedReferences
-from ._layout import Layout, Header, Drawer, Page, Footer, Toolbar, ToolbarTitle, Icon, Space
+import base64
+from io import BytesIO
+from typing import TYPE_CHECKING
+
+try:
+    # noinspection PyPackageRequirements
+    import mpld3 as mpld3
+    MPLD3 = True
+except ImportError:
+    MPLD3 = False
+
+try:
+    # noinspection PyPackageRequirements
+    import matplotlib.pyplot as plt
+    import matplotlib
+    # ref. https://stackoverflow.com/a/65793660/1031191
+    matplotlib.use('Agg')  # if plt.subplots opens a window, this gui crashes, so we disable matplotlib gui.
+    MATPLOTLIB = True
+except ImportError:
+    MATPLOTLIB = False
+
+from ._base import Component
 # noinspection PyUnresolvedReferences
 from ._form import Button, Input
-from ._tools import merge_classes
+# noinspection PyUnresolvedReferences
+from ._layout import Layout, Header, Drawer, Page, Footer, Toolbar, ToolbarTitle, Icon, Space
+from ._tools import merge_classes, str_between
 from .typing import *
+
+if TYPE_CHECKING:
+    from matplotlib.figure import Figure
 
 
 class Div(Component):
@@ -112,4 +136,55 @@ class Columns(Div):
 
 
 class Plot(Component):
-    pass
+    def __init__(self,
+                 interactive: bool = True,
+                 classes: ClassesType = None,
+                 styles: StylesType = None
+                 ):
+        self.interactive = interactive
+        if interactive:
+            if not MPLD3:
+                raise ImportError("Please install mpld3 package to use interactive plots")
+        else:
+            if not MATPLOTLIB:
+                raise ImportError("Please install matplotlib package to use interactive plots")
+
+        self.html = {}
+        self.img_base64 = ''
+        super().__init__(classes=classes, styles=styles)
+
+    def set_figure(self, fig: 'Figure', ax):
+        self.html = {}
+        self.img_base64 = ''
+        if self.interactive:
+            raw_html = mpld3.fig_to_html(fig)
+            self.html['div'] = str_between(raw_html, "<div>", "</div>")
+            self.html['script'] = str_between(raw_html, "<script>", "</script>")
+            self.html['style'] = str_between(raw_html, "<style>", "</style>")
+        else:
+            tmpfile = BytesIO()
+            fig.savefig(tmpfile, format='png')
+            encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
+            self.img_base64 = "data:image/png;base64,{}".format(encoded)
+        self.update()
+
+    @property
+    def vue(self) -> dict:
+        if self.html:
+            return self._merge_vue({
+                'component': 'mpld3-figure',
+                'children': [self.html['div']],
+                'props': {
+                    'script': self.html['script'],
+                    'style': self.html['style']
+                }
+            })
+        elif self.img_base64:
+            return self._merge_vue({
+                'component': 'q-img',
+                'props': {'src': self.img_base64}
+            })
+        else:
+            return self._merge_vue({
+                'component': 'div'
+            })
