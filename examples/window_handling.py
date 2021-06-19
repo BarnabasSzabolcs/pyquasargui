5709e050a5ef9a5709d2208521ba647028550a69
Szabolcs Barnabás <szabolcs.barnabas@gmail.com>
2021-06-19 21:27:42 +0200

    adds native dialog demo

--- examples/window_handling.py
+++ examples/window_handling.py
@@ -7,7 +7,7 @@ from quasargui.main import create_window
 
 def create_new_window():
     layout = create_layout('New window')
-    create_window(layout, position=(randint(1, 100), randint(1, 100)), size=(400, 400))
+    create_window(layout, position=(randint(1, 100), randint(1, 100)), size=(400, 500))
     sleep(0.5)
     layout.api.plugins.notify(message='A brand new window!', type='positive')
 
@@ -22,6 +22,21 @@ def toggle_fullscreen(layout, model):
     layout.api.toggle_fullscreen()
 
 
+def save_dialog(api):
+    result = api.show_save_dialog()
+    api.plugins.notify(message='Result: {}'.format(result))
+
+
+def open_dialog(api):
+    result = api.show_open_dialog()
+    api.plugins.notify(message='Result: {}'.format(result))
+
+
+def folder_dialog(api):
+    result = api.show_folder_dialog()
+    api.plugins.notify(message='Result: {}'.format(result))
+
+
 def create_layout(title):
     title_model = Model(title)
     title_model.add_callback(
@@ -34,7 +49,7 @@ def create_layout(title):
         lambda: width.api.resize_window((width.value, None)) if 300 <= width.value <= 1000 else None,
         immediate=True
     )
-    height = Model(400)
+    height = Model(500)
     height.add_callback(
         lambda: height.api.resize_window((None, height.value)) if 300 <= height.value <= 1000 else None,
         immediate=True
@@ -56,9 +71,12 @@ def create_layout(title):
         QPage([
             Rows([
                 InputStr('Window title', title_model),
-                QButton('Create new window', events={'click': create_new_window}),
                 QInput('Resize window width', width, type='number'),
                 QInput('Resize window height', height, type='number'),
+                QButton('Create new window', events={'click': create_new_window}),
+                QButton('Open file dialog', events={'click': lambda: open_dialog(layout.api)}),
+                QButton('Open folder dialog', events={'click': lambda: folder_dialog(layout.api)}),
+                QButton('Open save dialog', events={'click': lambda: save_dialog(layout.api)}),
             ])
         ])
     ])
@@ -66,4 +84,4 @@ def create_layout(title):
 
 
 layout = create_layout('Main window')
-run(layout, size=(400, 400))
+run(layout, size=(400, 500))
